399259bd281fd110fe9a3f433dd5943a9cf00412
perforb <dev.perfumed.garden@gmail.com>
2020-07-31 20:42:41 +0900

    Removed scanBasePackages

--- api/src/main/java/com/example/AwesomeApplication.java
+++ api/src/main/java/com/example/AwesomeApplication.java
@@ -3,7 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example")
+@SpringBootApplication
 public class AwesomeApplication {
 
 	public static void main(String[] args) {
